33c7d06238f675480fbf87a9e4db28cae6e34ce4
Justin Santos <jgsant20@gmail.com>
2020-11-08 10:17:24 -0600

    changes

--- py_execute_command_service.py
+++ py_execute_command_service.py
@@ -6,16 +6,51 @@ class PyExecuteCommandService(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
   
+  def _parse_commmand(self, command):
+    command = "\\n".join(command.splitlines()[1:])
+
+    # strip code block
+    if command[-3:] == '```':
+      command = command[5:-5]
+
+    command = command.replace('print', 'result.append')
+
+    return command
+
   @commands.command()
   async def py(self, ctx, *, args):
     if not str(ctx.message.author.id) == config.ADMIN_ID:
       await ctx.send('You\'re not Lyp, get away >:( {}')
       return
 
-    command = args
+    command = self._parse_commmand(args)
+    result = []
 
     try:
-      result = eval(command)
+      await exec(command)
       await ctx.send(result)
     except Exception as e:
       await ctx.send('\'{}\': {}'.format(command, e))
+
+# def _parse_commmand(command):
+#     command = "\\n".join(command.splitlines()[1:])
+
+#     # strip code block
+#     if command[-3:] == '```':
+#       command = command[5:-5]
+
+#     command = command.replace('print', 'result.append')
+
+#     return command
+
+
+# y = """
+# ```
+# for i in range(10):
+#   print(x)
+# ```
+# """
+
+# x = _parse_commmand(y)
+
+# print(x)
